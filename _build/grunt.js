debd15f867716b868cf6e0e9e260aa89fcfdc609
Takazudo <takazudo@gmail.com>
2012-03-29 22:00:47 +0900

    refactored build utils

--- _build/grunt.js
+++ _build/grunt.js
@@ -3,13 +3,8 @@
  * This compiles coffee to js
  *
  * grunt: https://github.com/cowboy/grunt
- * CoffeeScript: http://coffeescript.org/
- * growlnotify: http://growl.info/extras.php#growlnotify
  */
 module.exports = function(grunt){
-
-  var proc = require('child_process');
-  var log = grunt.log;
 
   grunt.initConfig({
     pkg: '<json:info.json>',
@@ -38,43 +33,12 @@ module.exports = function(grunt){
         '../tests/qunit/test/test.coffee'
       ]
     },
-    min: {
+    uglify: {
       '../jquery.ui.domwindow.min.js': '../jquery.ui.domwindow.js'
     }
   });
 
-  grunt.registerMultiTask('min', 'minify', function() {
-    var done = this.async();
-    var src = this.file.src;
-    var dest = this.file.dest;
-    var command = 'uglifyjs -o ' + dest + ' ' + src;
-    var out = proc.exec(command, function(err, sout, serr){
-      log.writeln('uglified.');
-      done(true);
-    });
-  });
-
-  grunt.registerMultiTask('coffee', 'compile CoffeeScripts', function() {
-    var done = this.async();
-    var srcs = this.file.src.join(' ');
-    var dest = this.file.dest;
-    var command = 'coffee --join ' + dest + ' --compile ' + srcs;
-    var out = proc.exec(command, function(err, sout, serr){
-      if(err || sout || serr){
-        proc.exec("growlnotify -t 'COFFEE COMPILE ERROR!' -m '" + serr + "'");
-        log.writeln('Scripts were failed to compile to ' + dest + '.');
-        done(false);
-      }else{
-        log.writeln('Scripts in were compiled to ' + dest + '.');
-        done(true);
-      }
-    });
-  });
-
-  grunt.registerTask('notifyOK', 'done!', function(){
-    proc.exec("growlnotify -t 'grunt.js' -m '＼(^o^)／'");
-  });
-
+  grunt.loadTasks('tasks');
   grunt.registerTask('default', 'coffee concat notifyOK');
 
 };
