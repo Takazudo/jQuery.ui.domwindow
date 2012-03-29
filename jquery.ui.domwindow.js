245109588d0da25ecd01f4183bcdbd0b2aa81eb6
Takazudo <takazudo@gmail.com>
2012-03-29 19:58:50 +0900

    bumped version number to 0.1.2

--- jquery.ui.domwindow.js
+++ jquery.ui.domwindow.js
@@ -1,4 +1,4 @@
-/*! jQuery.ui.domwindow - v0.1.1 -  3/29/2012
+/*! jQuery.ui.domwindow - v0.1.2 -  3/29/2012
  * https://github.com/Takazudo/jQuery.ui.domwindow
  * Copyright (c) 2012 "Takazudo" Takeshi Takatsudo; Licensed MIT */
 
